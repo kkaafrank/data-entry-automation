848e8848ec07a1f1099fd41491e7d8937c4e87ef
kkaafrank <59890581+kkaafrank@users.noreply.github.com>
2023-07-02 19:23:17 -0500

    update build script (add exe to source control)

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-py -m PyInstaller --noconfirm --log-level ERROR .\automation_gui.py -n claim_entry_automation
-COPY .env.shared dist\claim_entry_automation\.env.shared
-COPY .env dist\claim_entry_automation\.env
+py -m PyInstaller --noconfirm --log-level ERROR --onefile .\automation_gui.py -n claim_entry_automation
+COPY dist\claim_entry_automation.exe claim_entry_automation.exe
